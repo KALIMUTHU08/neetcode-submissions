74ae797723a720d60828130994b3dc383782da08
KALIMUTHU08 <anandkali6767@gmail.com>
2026-09-18 15:32:22 +0530

    Time: 1 ms (98.42%) | Memory: 42.1 MB (99.05%) - LeetSync

---
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int sum = 0;
+        while(n!=0){
+            int a = n%10;
+            sum+=a;
+            n = n/10;
+        }
+        return sum;
+    }
+}
